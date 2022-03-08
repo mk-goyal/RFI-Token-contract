67fe081d599543e941ed6a4a3494d7914a3a4f80
dperk6 <daniel@perklin.ca>
2022-03-08 16:08:20 -0500

    function to uniswap interface for remix testing

--- contracts/FrankInuTokenOneFile.sol
+++ contracts/FrankInuTokenOneFile.sol
@@ -134,6 +134,13 @@ interface IUniswapV2Router {
             uint256 liquidity
         );
 
+    function swapExactETHForTokensSupportingFeeOnTransferTokens(
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external payable;
+
     function swapExactTokensForETHSupportingFeeOnTransferTokens(
         uint256 amountIn,
         uint256 amountOutMin,
